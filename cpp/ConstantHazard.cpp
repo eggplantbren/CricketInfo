5a106c6bec500b469a2772ddb6f663df3af9fc53
Brendon J. Brewer <brendon.brewer@gmail.com>
2017-11-14 13:02:08 +1300

    It runs!

--- cpp/ConstantHazard.cpp
+++ cpp/ConstantHazard.cpp
@@ -1,4 +1,5 @@
 #include "ConstantHazard.h"
+#include <Utils.h>
 
 namespace CricketInfo
 {
@@ -27,6 +28,53 @@ void ConstantHazard::compute_xs()
 
     for(size_t i=0; i<N; ++i)
         xs[i] = static_cast<int>(log(1.0 - us[i])*val);
+}
+
+double ConstantHazard::perturb(InfoNest::RNG& rng)
+{
+    double logH = 0.0;
+
+    // Different kinds of proposal
+    int which = rng.rand_int(3);
+
+    if(which == 0)
+    {
+        // Perturb mu
+        mu = log(mu);
+        logH -= -0.5*pow((mu - log(25.0)) / 0.75, 2);
+        mu += 0.75*rng.randh();
+        logH += -0.5*pow((mu - log(25.0)) / 0.75, 2);
+        mu = exp(mu);
+    }
+    else if(which == 1)
+    {
+        // Regenerate some of the us
+        int reps = static_cast<int>(pow(static_cast<double>(N), rng.rand()));
+        for(int i=0; i<reps; ++i)
+            us[rng.rand_int(N)] = rng.rand();
+        compute_xs();
+    }
+    else
+    {
+        // Perturb a single u
+        int i = rng.rand_int(N);
+        us[i] += rng.randh();
+        InfoNest::wrap(us[i], 0.0, 1.0);
+        compute_xs();        
+    }
+
+    return logH;
+}
+
+void ConstantHazard::print(std::ostream& out) const
+{
+    out << mu << ' ';
+}
+
+double ConstantHazard::parameter_distance(const ConstantHazard& s1,
+                                          const ConstantHazard& s2)
+{
+    return std::abs(s1.mu - s2.mu);
 }
 
 } // namespace CricketInfo
--- cpp/main-constanthazard.cpp
+++ cpp/main-constanthazard.cpp
@@ -1,13 +1,31 @@
+#include <ctime>
 #include <iostream>
+#include <Execute.hpp>
 #include "ConstantHazard.h"
 #include "RNG.h"
 
 int main()
 {
-    InfoNest::RNG rng(0);
+    // Create random number generators
+    // The first one is used to generate reference points
+    // and the second is used for the Nested Sampling
+    unsigned long seed0 = 0;
+    unsigned long seed1 = time(0);
+    InfoNest::RNG rng0(seed0);
+    InfoNest::RNG rng1(seed1);
 
-    CricketInfo::ConstantHazard c;
-    c.generate(rng);
+    // Define run parameters
+    constexpr double depth         = 1000.0;
+    constexpr size_t num_reps      = 1000;
+    constexpr size_t num_particles = 10;
+    constexpr size_t mcmc_steps    = 10000;
+
+    // Do the run.
+    InfoNest::execute<CricketInfo::ConstantHazard>
+                        (rng0, rng1,
+                         depth, num_reps, num_particles, mcmc_steps,
+                         CricketInfo::ConstantHazard::parameter_distance);
+
 
     return 0;
 }
